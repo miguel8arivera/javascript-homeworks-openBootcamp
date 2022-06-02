5f677f67e4b2f8506be972ec6fee3cec8d3a56e1
Miguel Angel <miguel.angel.8a.rivera@gmail.com>
2022-06-02 07:39:17 -0500

    02-trabajo-js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+//? Primer Trabajo
+
 console.log(
   "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo."
 );
+
+//? Segundo Trabajo
+
+const user = ["Miguel", 27, true, 28, 10, 1995];
+
+console.log(user);
+
+const libro = {
+  titulo: "El señor de los anillos",
+  author: "J.R.R. Tolkien",
+  date: "1954-09-29",
+  url: "https://www.google.com/search?q=el+senor+de+los+anillos&rlz=1C1CHBF_esES831ES831&oq=el+senor+de+los+anillos&aqs=chrome..69i57j0l5.3168j0j7&sourceid=chrome&ie=UTF-8",
+};
+
+console.log(libro);
